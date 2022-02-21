11b85c63283d15aef2a51549ba743fce10ec13b8
Michael <michaelhiggins285@gmail.com>
2022-02-21 11:34:21 +0000

    wrote seed functions for all tables

--- db/data/test-data/index.ts
+++ db/data/test-data/index.ts
@@ -1,5 +1,9 @@
 import { userData } from './users'
 import { goalData } from './goals'
 import { subgoalData } from './subgoals'
+import { postData } from './posts'
+import { commentData } from './comments'
+import { reactionData } from './reactions'
+import { friendshipData } from './friendships'
 
-export { userData, goalData, subgoalData }
+export { userData, goalData, subgoalData, postData, commentData, reactionData, friendshipData }
