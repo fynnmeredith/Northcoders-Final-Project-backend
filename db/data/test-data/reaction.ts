7a95d7e7d24fd0c90cd8e77ffe18828c50a41e76
Joe Valentine <joe.valentine@live.co.uk>
2022-02-21 11:05:48 +0000

    push test

--- db/data/test-data/reaction.ts
+++ db/data/test-data/reaction.ts
@@ -5,31 +5,37 @@
     {
         reaction_id:1,
         post_id:1,
-        owner:'You can do it',
+        owner:'susan',
         reaction:'Like'
     },
     {
-        reaction_id:1,
+        reaction_id:2,
         post_id:1,
-        owner:'You can do it',
+        owner:'mary',
         reaction:'Dislike'
     },
     {
-        reaction_id:1,
+        reaction_id:3,
         post_id:1,
-        owner:'You can do it',
+        owner:'stuart',
         reaction:'Star'
     },
     {
-        reaction_id:1,
-        post_id:1,
-        owner:'You can do it',
+        reaction_id:4,
+        post_id:2,
+        owner:'jeff',
         reaction:'Like'
     },
     {
-        reaction_id:1,
-        post_id:1,
-        owner:'You can do it',
+        reaction_id:5,
+        post_id:2,
+        owner:'susan',
         reaction:'Like'
+    },
+    {
+        reaction_id:5,
+        post_id:2,
+        owner:'branch push test',
+        reaction:'branch push test'
     }
 ]
