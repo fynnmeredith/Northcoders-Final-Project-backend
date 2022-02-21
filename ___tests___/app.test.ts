b268d00656ea733d6db9f3ec7d7e502759a1bdc2
Michael <michaelhiggins285@gmail.com>
2022-02-21 12:33:59 +0000

    added prettier formatting to two files

--- ___tests___/app.test.ts
+++ ___tests___/app.test.ts
@@ -1,49 +1,35 @@
-import { db } from '../db/connection'
-import * as testData from '../db/data/test-data/index'
-import { seed } from '../db/seeds/seed'
-import { app } from '../app'
-import * as request from 'supertest'
-import {} from 'ts-jest'
+import { db } from "../db/connection";
+import * as testData from "../db/data/test-data/index";
+import { seed } from "../db/seeds/seed";
+import { app } from "../app";
+import * as request from "supertest";
+import {} from "ts-jest";
 
-beforeEach(() => seed(testData))
+beforeEach(() => seed(testData));
 
-describe('', () => {
+describe("", () => {
+  describe("seed works correctly", () => {
+    test("subgoal table correctly seeded", () => {
+      return db.query(`SELECT * FROM subgoals;`).then((res) => {
+        console.log(res.rows);
+        expect(res.rows.length).toBe(8);
 
-    describe('seed works correctly', () => {
+        res.rows.forEach((subgoal) => {
+          expect(subgoal).toEqual(
+            expect.objectContaining({
+              subgoal_id: expect.any(Number),
+              goal_id: expect.any(Number),
+              objective: expect.any(String),
+              end_date: expect.any(Date),
+              type: expect.any(String),
+              status: expect.any(String),
+              owner: expect.any(String),
+            })
+          );
+        });
+      });
+    });
+  });
+});
 
-        test('subgoal table correctly seeded', () => {
-    
-            return db.query(`SELECT * FROM subgoals;`)
-                .then((res) => {
-
-                    console.log(res.rows)
-    
-                    expect(res.rows.length).toBe(8)
-    
-                    res.rows.forEach((subgoal) => {
-    
-                        expect(subgoal).toEqual(
-                        expect.objectContaining({
-    
-                            subgoal_id: expect.any(Number),
-                            goal_id: expect.any(Number),
-                            objective: expect.any(String),
-                            end_date: expect.any(Date),
-                            type: expect.any(String), 
-                            status: expect.any(String), 
-                            owner: expect.any(String)
-
-                        })
-                        )
-    
-                    })
-    
-                })
-    
-        })
-    
-    })
-
-})
-
-export {}
+export {};
--- db/seeds/seed.ts
+++ db/seeds/seed.ts
@@ -1,34 +1,56 @@
-import { db } from "../connection"
-import format from "pg-format"
-import { formatUsers, formatGoals, formatSubgoals, formatPosts, formatComments, formatReactions, formatFriendships } from '../../utils/format'
+import { db } from "../connection";
+import format from "pg-format";
+import {
+  formatUsers,
+  formatGoals,
+  formatSubgoals,
+  formatPosts,
+  formatComments,
+  formatReactions,
+  formatFriendships,
+} from "../../utils/format";
 
 const seed = (data) => {
+  const {
+    userData,
+    goalData,
+    subgoalData,
+    postData,
+    commentData,
+    reactionData,
+    friendshipData,
+  } = data;
 
-  const { userData, goalData, subgoalData, postData, commentData, reactionData, friendshipData } = data
-  
-  return db
-    .query('DROP TABLE IF EXISTS friendships;')
-    .then(() => {
-      return db.query('DROP TABLE IF EXISTS reactions;')})
-    .then(() => {
-      return db.query('DROP TABLE IF EXISTS comments;')})
-    .then(() => {
-      return db.query('DROP TABLE IF EXISTS posts;')})
-    .then(() => {
-      return db.query('DROP TABLE IF EXISTS subgoals;')})
-    .then(() => {
-      return db.query('DROP TABLE IF EXISTS goals;')})
-    .then(() => {
-      return db.query('DROP TABLE IF EXISTS users;')})
-    .then(() => {
-      return db.query(`
+  return (
+    db
+      .query("DROP TABLE IF EXISTS friendships;")
+      .then(() => {
+        return db.query("DROP TABLE IF EXISTS reactions;");
+      })
+      .then(() => {
+        return db.query("DROP TABLE IF EXISTS comments;");
+      })
+      .then(() => {
+        return db.query("DROP TABLE IF EXISTS posts;");
+      })
+      .then(() => {
+        return db.query("DROP TABLE IF EXISTS subgoals;");
+      })
+      .then(() => {
+        return db.query("DROP TABLE IF EXISTS goals;");
+      })
+      .then(() => {
+        return db.query("DROP TABLE IF EXISTS users;");
+      })
+      .then(() => {
+        return db.query(`
       CREATE TABLE users (
         username VARCHAR(25) PRIMARY KEY,
         profile VARCHAR(300)
-      );`)
-    })
-    .then(() => {
-      return db.query(`
+      );`);
+      })
+      .then(() => {
+        return db.query(`
       CREATE TABLE goals (
         goal_id SERIAL PRIMARY KEY,
         objective VARCHAR(200) NOT NULL,
@@ -44,10 +66,10 @@ const seed = (data) => {
         finish_date DATE,
         FOREIGN KEY (owner)
         REFERENCES users(username)
-      );`)
-    })
-    .then(() => {
-      return db.query(`
+      );`);
+      })
+      .then(() => {
+        return db.query(`
       CREATE TABLE subgoals (
         subgoal_id SERIAL PRIMARY KEY,
         goal_id INTEGER NOT NULL,
@@ -65,10 +87,10 @@ const seed = (data) => {
         REFERENCES users(username),
         FOREIGN KEY (goal_id)
         REFERENCES goals(goal_id)
-      );`)
-    })
-    .then(() => {
-      return db.query(`
+      );`);
+      })
+      .then(() => {
+        return db.query(`
       CREATE TABLE posts (
         post_id SERIAL PRIMARY KEY,
         associated_data_type VARCHAR(10) NOT NULL,
@@ -78,10 +100,10 @@ const seed = (data) => {
         message VARCHAR(2000),
         FOREIGN KEY (owner)
         REFERENCES users(username)
-      );`)
-    })
-    .then(() => {
-      return db.query(`
+      );`);
+      })
+      .then(() => {
+        return db.query(`
       CREATE TABLE comments (
         comment_id SERIAL PRIMARY KEY,
         post_id INTEGER NOT NULL,
@@ -92,10 +114,10 @@ const seed = (data) => {
         REFERENCES posts(post_id),
         FOREIGN KEY (owner)
         REFERENCES users(username)
-      );`)
-    })
-    .then(() => {
-      return db.query(`
+      );`);
+      })
+      .then(() => {
+        return db.query(`
       CREATE TABLE reactions (
         reaction_id SERIAL PRIMARY KEY,
         post_id INTEGER NOT NULL,
@@ -105,88 +127,88 @@ const seed = (data) => {
         REFERENCES posts(post_id),
         FOREIGN KEY (owner)
         REFERENCES users(username)
-      );`)
-    })
-    .then(() => {
-      return db.query(`
+      );`);
+      })
+      .then(() => {
+        return db.query(`
       CREATE TABLE friendships (
         friendship_id SERIAL PRIMARY KEY,
         user_1 VARCHAR(25) NOT NULL,
         user_2 VARCHAR(25) NOT NULL
-      );`)
-    })
-    .then(() => {
-      const query = format(
-        `INSERT INTO users
+      );`);
+      })
+      .then(() => {
+        const query = format(
+          `INSERT INTO users
           (username, profile)
           VALUES
           %L;`,
-        formatUsers(userData)
-      );
-      return db.query(query);
-    })
-    .then(() => {
-      const query = format(
-        `INSERT INTO goals
+          formatUsers(userData)
+        );
+        return db.query(query);
+      })
+      .then(() => {
+        const query = format(
+          `INSERT INTO goals
           (objective, description, start_date, end_date, type, status, owner, target_value, unit, progress, finish_date)
           VALUES
           %L;`,
-        formatGoals(goalData)
-      );
-      return db.query(query);
-    })
-    .then(() => {
-      const query = format(
-        `INSERT INTO subgoals
+          formatGoals(goalData)
+        );
+        return db.query(query);
+      })
+      .then(() => {
+        const query = format(
+          `INSERT INTO subgoals
           (goal_id, objective, start_date, end_date, type, status, owner, target_value, unit, progress, finish_date)
           VALUES
           %L;`,
-        formatSubgoals(subgoalData)
-      );
-      return db.query(query);
-    })
-    // RE-ENTER DATETIMES
-    .then(() => {
-      const query = format(
-        `INSERT INTO posts
+          formatSubgoals(subgoalData)
+        );
+        return db.query(query);
+      })
+      // RE-ENTER DATETIMES
+      .then(() => {
+        const query = format(
+          `INSERT INTO posts
           (associated_data_type, associated_id, owner, message)
           VALUES
           %L;`,
-        formatPosts(postData)
-      );
-      return db.query(query);
-    })
-    .then(() => {
-      const query = format(
-        `INSERT INTO comments
+          formatPosts(postData)
+        );
+        return db.query(query);
+      })
+      .then(() => {
+        const query = format(
+          `INSERT INTO comments
           (post_id, owner, message)
           VALUES
           %L;`,
-        formatComments(commentData)
-      );
-      return db.query(query);
-    })
-    .then(() => {
-      const query = format(
-        `INSERT INTO reactions
+          formatComments(commentData)
+        );
+        return db.query(query);
+      })
+      .then(() => {
+        const query = format(
+          `INSERT INTO reactions
           (post_id, owner, reaction)
           VALUES
           %L;`,
-        formatReactions(reactionData)
-      );
-      return db.query(query);
-    })
-    .then(() => {
-      const query = format(
-        `INSERT INTO friendships
+          formatReactions(reactionData)
+        );
+        return db.query(query);
+      })
+      .then(() => {
+        const query = format(
+          `INSERT INTO friendships
           (user_1, user_2)
           VALUES
           %L;`,
-        formatFriendships(friendshipData)
-      );
-      return db.query(query);
-    })
-    
+          formatFriendships(friendshipData)
+        );
+        return db.query(query);
+      })
+  );
 };
 
-export { seed }
+export { seed };
