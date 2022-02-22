5ddfcce362f037022a77beac9695e58f0f7b823a
Joe Valentine <joe.valentine@live.co.uk>
2022-02-22 16:15:37 +0000

    heroku hosting

--- listen.ts
+++ listen.ts
@@ -1,7 +1,4 @@
 import { app } from "./app";
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, (err) => {
-  if (err) throw err;
-  console.log(`Listening on ${PORT}...`);
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
