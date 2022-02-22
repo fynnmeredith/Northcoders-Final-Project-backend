d4b3714a4b9fc43c17d13b853bb96f54ac7d4ea6
Joe Valentine <joe.valentine@live.co.uk>
2022-02-22 14:54:12 +0000

    post and patch user endpoints complete

--- models/users.model.ts
+++ models/users.model.ts
@@ -1,19 +1,43 @@
-import {db} from"../db/connection"
+import { db } from "../db/connection";
 
-export const selectUsers = () =>{
-
-    return db.query(`SELECT * FROM users`)
-    .then((res)=>{
-        return res.rows
+export const selectUsers = () => {
+  return db
+    .query(`SELECT * FROM users;`)
+    .then((res) => {
+      return res.rows;
     })
+    .catch((err) => {
+      return err;
+    });
+};
 
-}
+export const insertUser = (username, profile) => {
+  return db
+    .query(`INSERT INTO users (username,profile) VALUES ($1,$2) RETURNING *`, [
+      username,
+      profile,
+    ])
+    .then((res) => {
+      // console.log(res.rows);
+      return res.rows;
+    });
+};
 
-export const insertUser = (username,profile) => {
-    
-    return db.query(`INSERT INTO users (username,profile) VALUES ($1,$2) RETURNING *`,[username,profile])
-    .then((res)=>{
-        return res.rows
-    })
+export const modifyUser = (username, profile) => {
+  return db
+    .query(
+      `UPDATE users SET profile = ($2) WHERE  username =($1) RETURNING *;`,
+      [username, profile]
+    )
+    .then((res) => {
+      return res.rows;
+    });
+};
 
-}
+export const deleteUserModel = (username) => {
+  return db
+    .query(`DELETE FROM users WHERE username=($1) RETURNING *;`, [username])
+    .then((res) => {
+      return res.rows;
+    });
+};
--- routers/users.routers.ts
+++ routers/users.routers.ts
@@ -1,15 +1,21 @@
-import express from 'express'
+import express from "express";
 // import { getGoalsByUser } from "../controllers/goals.controller";
 
 import {
   getUsers,
   postUser,
+  patchUser,
+  deleteUser,
 } from "../controllers/users.controller";
 
 const usersRouter = express.Router();
 
-usersRouter.route("/").get(getUsers).post(postUser)
+usersRouter
+  .route("/")
+  .get(getUsers)
+  .post(postUser)
+  .patch(patchUser)
+  .delete(deleteUser);
 // usersRouter.route("/").get(getUsers).post(postUser).get("/:username/goals", getGoalsByUser*/);
 
-
-export {usersRouter};
+export { usersRouter };
