1b6241e4910742adf44682ec4ae765a3fb1bb6d6
Joe Valentine <joe.valentine@live.co.uk>
2022-02-23 10:02:47 +0000

    heroku fix

--- heroku/db/connection.js
+++ heroku/db/connection.js
@@ -3,20 +3,19 @@ exports.__esModule = true;
 exports.db = void 0;
 var pg_1 = require("pg");
 var ENV = process.env.NODE_ENV || "development";
-var config =
-  ENV === "production"
+var config = ENV === "production"
     ? {
         connectionString: process.env.DATABASE_URL,
         ssl: {
-          rejectUnauthorized: false,
-        },
-      }
+            rejectUnauthorized: false
+        }
+    }
     : {};
 require("dotenv").config({
-  path: "".concat(__dirname, "/../.env.").concat(ENV),
+    path: "".concat(__dirname, "/../.env.").concat(ENV)
 });
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
+    throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 var db = new pg_1.Pool(config);
 exports.db = db;
