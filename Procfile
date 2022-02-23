a7ebaa67214311c1c6f444a164e53328a5999a12
Michael <michaelhiggins285@gmail.com>
2022-02-23 10:59:07 +0000

    incorporate heroku changes to MAH-Branch-4

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node heroku/listen.js
---
+++ db/data/test-data/reaction.ts
@@ -0,0 +1,41 @@
+//jeff,susan,mary,stuart,mahood,martina,dmitri,betty
+
+
+[
+    {
+        reaction_id:1,
+        post_id:1,
+        owner:'susan',
+        reaction:'Like'
+    },
+    {
+        reaction_id:2,
+        post_id:1,
+        owner:'mary',
+        reaction:'Dislike'
+    },
+    {
+        reaction_id:3,
+        post_id:1,
+        owner:'stuart',
+        reaction:'Star'
+    },
+    {
+        reaction_id:4,
+        post_id:2,
+        owner:'jeff',
+        reaction:'Like'
+    },
+    {
+        reaction_id:5,
+        post_id:2,
+        owner:'susan',
+        reaction:'Like'
+    },
+    {
+        reaction_id:5,
+        post_id:2,
+        owner:'branch push test',
+        reaction:'branch push test'
+    }
+]
